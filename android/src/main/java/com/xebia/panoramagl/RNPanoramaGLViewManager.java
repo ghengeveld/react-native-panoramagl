dd50004c6f434aeb02dcb3fe95d6df0014c6c9b9
Gert Hengeveld <info@ghengeveld.nl>
2016-10-07 10:12:35 +0200

    Fix compile error.

--- android/src/main/java/com/xebia/panoramagl/RNPanoramaGLViewManager.java
+++ android/src/main/java/com/xebia/panoramagl/RNPanoramaGLViewManager.java
@@ -2,6 +2,7 @@ package com.xebia.panoramagl;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.SimpleViewManager;
+import com.facebook.react.uimanager.ThemedReactContext;
 
 public class RNPanoramaGLViewManager extends SimpleViewManager<RNPanoramaGLView> {
     private static final String REACT_CLASS = "RNPanoramaGL";
@@ -16,6 +17,11 @@ public class RNPanoramaGLViewManager extends SimpleViewManager<RNPanoramaGLView>
     @Override
     public String getName() {
         return REACT_CLASS;
+    }
+
+    @Override
+    public RNPanoramaGLView createViewInstance (ThemedReactContext context) {
+        return new RNPanoramaGLView(context, this);
     }
 
     public ReactApplicationContext getContext() {
